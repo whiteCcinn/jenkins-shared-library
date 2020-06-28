b499f5fc4e037382d0c31871520be02bb08038cf
whiteCcinn <471113744@qq.com>
2020-06-28 14:44:40 +0800

    update

--- src/org/mingchao/Point.groovy
+++ src/org/mingchao/Point.groovy
@@ -3,4 +3,14 @@ package org.mingchao;
 // point in 3D space
 class Point {
   float x,y,z;
+
+  Point(x,y,z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  int Total() {
+    return this.x + this.y + this.z;
+  }
 }
